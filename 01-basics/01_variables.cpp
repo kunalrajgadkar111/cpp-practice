9a201de9cf687c4ab82c09f8c0319e5cc1a3bd0a
kunalrajgadkar111 <66273460+kunalrajgadkar111@users.noreply.github.com>
2026-09-19 22:38:23 +0530

    Add C++ variables practice example
    This file demonstrates the use of different variable types in C++.

---
+++ 01-basics/01_variables.cpp
@@ -0,0 +1,22 @@
+// C++ Variables Practice
+// This file contains basic examples of variables in C++.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int age = 34;
+    float height = 5.5;
+    char grade = 'A';
+    string name = "Kunal";
+    bool isStudent = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
